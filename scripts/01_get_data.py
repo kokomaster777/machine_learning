01cbfc9a81c726883234e5fd8551eb794a72af3c
kokomaster777 <kostyaKoko26@yandex.ru>
2025-04-20 13:23:23 +0500

    Update 01_get_data.py

--- scripts/01_get_data.py
+++ scripts/01_get_data.py
@@ -1,5 +1,11 @@
+import os
 import pandas as pd
 
+# Проверяем, существует ли папка data, если нет — создаем её
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+# Загружаем данные и сохраняем в файл
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
 df = pd.read_csv(url)
 df.to_csv("data/raw.csv", index=False)
