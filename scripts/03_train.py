96671c52ab197f45cb354c8a1911f45aa53543df
kokomaster777 <kostyaKoko26@yandex.ru>
2025-04-20 13:23:38 +0500

    Update 03_train.py

--- scripts/03_train.py
+++ scripts/03_train.py
@@ -1,12 +1,20 @@
+import os
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
+# Проверяем, существует ли папка models, если нет — создаем её
+if not os.path.exists('models'):
+    os.makedirs('models')
+
+# Загружаем данные
 X_train = pd.read_csv("data/X_train.csv")
 y_train = pd.read_csv("data/y_train.csv")
 
+# Обучаем модель
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-with open("model/model.pkl", "wb") as f:
+# Сохраняем модель в файл
+with open("models/model.pkl", "wb") as f:
     pickle.dump(model, f)
